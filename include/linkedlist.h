49b3757838c5e8a9d008728de1adfff18cec7d1b
Romain GOMES <romain.gomes1378@gmail.com>
2024-11-24 17:59:18 +0100

    update comments

--- include/linkedlist.h
+++ include/linkedlist.h
@@ -16,62 +16,42 @@ typedef struct LinkedList {
     ListNode *tail;           // Pointeur vers le dernier nœud dans la liste
 } LinkedList;
 
-/**
- * @brief Crée et initialise une nouvelle liste chaînée.
- *
- * @return Pointeur vers la structure LinkedList nouvellement créée.
- *         Retourne NULL si l'allocation de mémoire échoue.
- */
+//Crée et initialise une nouvelle liste chaînée.
+//return Pointeur vers la structure LinkedList nouvellement créée.
+//return NULL si l'allocation de mémoire échoue.
 LinkedList* createLinkedList();
 
-/**
- * @brief Ajoute un nouveau nœud avec les données fournies à la fin de la liste chaînée.
- *
- * @param list Pointeur vers la LinkedList.
- * @param data Pointeur vers les données à stocker dans le nouveau nœud.
- *
- * @return 1 si l'opération est réussie, 0 sinon.
- */
+//Ajoute un nouveau nœud avec les données fournies à la fin de la liste chaînée.
+//param list Pointeur vers la LinkedList.
+//param data Pointeur vers les données à stocker dans le nouveau nœud.
+//return 1 si l'opération est réussie, 0 sinon.
 int appendLinkedList(LinkedList* list, void* data);
 
-/**
- * @brief Libère tous les nœuds dans la liste chaînée et la liste elle-même.
- *
- * @param list Pointeur vers la LinkedList à libérer.
- */
+//Libère tous les nœuds dans la liste chaînée et la liste elle-même.
+//param list Pointeur vers la LinkedList à libérer.
 void freeLinkedList(LinkedList* list);
 
-/**
- * @brief Trouve le premier nœud contenant les données spécifiées.
- *
- * @param list Pointeur vers la LinkedList.
- * @param data Pointeur vers les données à rechercher.
- * @param compare Pointeur vers une fonction de comparaison.
- *                La fonction de comparaison doit retourner 0 si les données correspondent.
- *
- * @return Pointeur vers le ListNode contenant les données.
- *         Retourne NULL si les données ne sont pas trouvées.
- */
+//Trouve le premier nœud contenant les données spécifiées.
+//param list Pointeur vers la LinkedList.
+//param data Pointeur vers les données à rechercher.
+//param compare Pointeur vers une fonction de comparaison.
+//La fonction de comparaison doit retourner 0 si les données correspondent.
+//return Pointeur vers le ListNode contenant les données.
+//return NULL si les données ne sont pas trouvées.
 ListNode* findLinkedList(LinkedList* list, void* data, int (*compare)(void*, void*));
 
-/**
- * @brief Supprime le premier nœud contenant les données spécifiées.
- *
- * @param list Pointeur vers la LinkedList.
- * @param data Pointeur vers les données à supprimer.
- * @param compare Pointeur vers une fonction de comparaison.
- *                La fonction de comparaison doit retourner 0 si les données correspondent.
- *
- * @return 1 si un nœud a été supprimé, 0 sinon.
- */
+//Supprime le premier nœud contenant les données spécifiées.
+//param list Pointeur vers la LinkedList.
+//param data Pointeur vers les données à supprimer.
+//param compare Pointeur vers une fonction de comparaison.
+//La fonction de comparaison doit retourner 0 si les données correspondent.
+//return 1 si un nœud a été supprimé, 0 sinon.
+int removeLinkedList(LinkedList* list, void* data, int (*compare)(void*, void*));
 int removeLinkedList(LinkedList* list, void* data, int (*compare)(void*, void*));
 
-/**
- * @brief Fonction utilitaire pour itérer sur la liste chaînée et appliquer une fonction à chaque donnée des nœuds.
- *
- * @param list Pointeur vers la LinkedList.
- * @param func Pointeur vers une fonction qui prend un argument void*.
- */
+//Fonction utilitaire pour itérer sur la liste chaînée et appliquer une fonction à chaque donnée des nœuds.
+//param list Pointeur vers la LinkedList.
+//param func Pointeur vers une fonction qui prend un argument void*.
 void iterateLinkedList(LinkedList* list, void (*func)(void*));
 
 #endif // LINKEDLIST_H
